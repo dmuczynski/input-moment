c922a7182067674ca1b0817787a625ddba3de5a8
David Muczynski <david@muczynski.com>
2018-09-23 21:50:14 -0400

    added toggle for military time

--- src/input-moment.js
+++ src/input-moment.js
@@ -8,22 +8,36 @@ export default class InputMoment extends Component {
   static defaultProps = {
     prevMonthIcon: 'ion-ios-arrow-left',
     nextMonthIcon: 'ion-ios-arrow-right',
-    minStep: 1,
-    hourStep: 1
+    isMilitary: false,
+    initTab: 'date',
+    onSave: false,
+    minStep: 5,
+    hourStep: 1,
+  };
+
+  static initialTab = {
+    date: 0,
+    time: 1,
   };
 
   state = {
-    tab: 0
+    tab: 0,
   };
 
-  handleClickTab = (e, tab) => {
+  componentWillMount() {
+    const { initTab } = this.props;
+    this.setState( { tab: InputMoment.initialTab[ initTab ] } );
+  }
+
+  handleClickTab = ( e, tab ) => {
     e.preventDefault();
-    this.setState({ tab: tab });
+    this.setState( { tab: tab } );
   };
 
   handleSave = e => {
     e.preventDefault();
-    if (this.props.onSave) this.props.onSave();
+    this.props.onSave ? this.props.onSave() : () => {
+    };
   };
 
   render() {
@@ -33,59 +47,64 @@ export default class InputMoment extends Component {
       className,
       prevMonthIcon,
       nextMonthIcon,
+      isMilitary,
+      initTab,
       minStep,
       hourStep,
+      onChange,
       onSave,
       ...props
     } = this.props;
-    const cls = cx('m-input-moment', className);
+    const cls = cx( 'm-input-moment', className );
 
     return (
-      <div className={cls} {...props}>
-        <div className="options">
-          <button
-            type="button"
-            className={cx('ion-calendar im-btn', { 'is-active': tab === 0 })}
-            onClick={e => this.handleClickTab(e, 0)}
-          >
-            Date
-          </button>
-          <button
-            type="button"
-            className={cx('ion-clock im-btn', { 'is-active': tab === 1 })}
-            onClick={e => this.handleClickTab(e, 1)}
-          >
-            Time
-          </button>
-        </div>
+        <div className={cls} {...props}>
+          <div className="options">
+            <button
+                type="button"
+                className={cx( 'ion-calendar im-btn',
+                    { 'is-active': tab === 0 } )}
+                onClick={e => this.handleClickTab( e, 0 )}
+            >
+              Date
+            </button>
+            <button
+                type="button"
+                className={cx( 'ion-clock im-btn', { 'is-active': tab === 1 } )}
+                onClick={e => this.handleClickTab( e, 1 )}
+            >
+              Time
+            </button>
+          </div>
 
-        <div className="tabs">
-          <Calendar
-            className={cx('tab', { 'is-active': tab === 0 })}
-            moment={m}
-            onChange={this.props.onChange}
-            prevMonthIcon={this.props.prevMonthIcon}
-            nextMonthIcon={this.props.nextMonthIcon}
-          />
-          <Time
-            className={cx('tab', { 'is-active': tab === 1 })}
-            moment={m}
-            minStep={this.props.minStep}
-            hourStep={this.props.hourStep}
-            onChange={this.props.onChange}
-          />
-        </div>
+          <div className="tabs">
+            <Calendar
+                className={cx( 'tab', { 'is-active': tab === 0 } )}
+                moment={m}
+                onChange={this.props.onChange}
+                prevMonthIcon={this.props.prevMonthIcon}
+                nextMonthIcon={this.props.nextMonthIcon}
+            />
+            <Time
+                className={cx( 'tab', { 'is-active': tab === 1 } )}
+                moment={m}
+                isMilitary={isMilitary}
+                minStep={minStep}
+                hourStep={hourStep}
+                onChange={onChange}
+            />
+          </div>
 
-        {this.props.onSave ? (
-          <button
-            type="button"
-            className="im-btn btn-save ion-checkmark"
-            onClick={this.handleSave}
-          >
-            Save
-          </button>
-        ) : null}
-      </div>
+          {this.props.onSave ? (
+              <button
+                  type="button"
+                  className="im-btn btn-save ion-checkmark"
+                  onClick={this.handleSave}
+              >
+                Save
+              </button>
+          ) : null}
+        </div>
     );
   }
 }
--- src/time.js
+++ src/time.js
@@ -3,50 +3,75 @@ import React, { Component } from 'react';
 import InputSlider from 'react-input-slider';
 
 export default class extends Component {
+
+  static defaultProps = {
+    isMilitary: false,
+  };
+
+  constructor( props ) {
+    super( props );
+  }
+
   changeHours = pos => {
     const m = this.props.moment;
-    m.hours(pos.x);
-    this.props.onChange(m);
+    m.hours( pos.x );
+    this.props.onChange( m );
   };
 
   changeMinutes = pos => {
     const m = this.props.moment;
-    m.minutes(pos.x);
-    this.props.onChange(m);
+    m.minutes( pos.x );
+    this.props.onChange( m );
   };
 
   render() {
+
+    const { isMilitary } = this.props;
+
     const m = this.props.moment;
 
-    return (
-      <div className={cx('m-time', this.props.className)}>
-        <div className="showtime">
-          <span className="time">{m.format('HH')}</span>
-          <span className="separater">:</span>
-          <span className="time">{m.format('mm')}</span>
-        </div>
+    const getFormattedTime = hour => hour % 12 ? hour % 12 : 12;
 
-        <div className="sliders">
-          <div className="time-text">Hours:</div>
-          <InputSlider
-            className="u-slider-time"
-            xmin={0}
-            xmax={23}
-            xstep={this.props.hourStep}
-            x={m.hour()}
-            onChange={this.changeHours}
-          />
-          <div className="time-text">Minutes:</div>
-          <InputSlider
-            className="u-slider-time"
-            xmin={0}
-            xmax={59}
-            xstep={this.props.minStep}
-            x={m.minute()}
-            onChange={this.changeMinutes}
-          />
+    return (
+        <div className={cx( 'm-time', this.props.className )}>
+          <div className="showtime">
+            <span className="time">{isMilitary ?
+                m.format( 'HH' ) :
+                getFormattedTime( m.format( 'HH' ) )}</span>
+            <span className="separater">:</span>
+            <span className="time">{m.format( 'mm' )}</span>
+            {!isMilitary &&
+            <span>
+                <span className="separater"></span>
+                <span className="time">{Math.floor( m.format( 'HH' ) / 12 ) ?
+                    'PM' :
+                    'AM'}</span>
+              </span>
+            }
+
+          </div>
+
+          <div className="sliders">
+            <div className="time-text">Hours:</div>
+            <InputSlider
+                className="u-slider-time"
+                xmin={0}
+                xmax={23}
+                xstep={this.props.hourStep}
+                x={m.hour()}
+                onChange={this.changeHours}
+            />
+            <div className="time-text">Minutes:</div>
+            <InputSlider
+                className="u-slider-time"
+                xmin={0}
+                xmax={59}
+                xstep={this.props.minStep}
+                x={m.minute()}
+                onChange={this.changeMinutes}
+            />
+          </div>
         </div>
-      </div>
     );
   }
 }
