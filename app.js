b14186e2bb41ac138b7b50ee5fff594cf1f6c683
David Muczynski <david@muczynski.com>
2018-09-23 22:09:54 -0400

    update gh-pages 2018-09-24T02:09:54.040Z

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ class App extends Component {
           <InputMoment
             moment={this.state.m}
             onChange={this.handleChange}
+            minStep={5}
           />
         </form>
       </div>
